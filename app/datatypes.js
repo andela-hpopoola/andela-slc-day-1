1e1694cd9f81722b144f2f85e371f1a8835f2336
Haruna Popoola <haruna.popoola@yahoo.com>
2017-03-12 15:42:38 +0100

    Finished working on Datatypes.js

--- app/datatypes.js
+++ app/datatypes.js
@@ -3,51 +3,51 @@
 // datatypes function
 var dataTypes = function(value){
 
-	// // use a switch case; if is messy
-	// switch (typeof value) {
+	// use a switch case; if is messy
+	switch (typeof value) {
 
-	// 	// value is a string; return string length
-	// 	case 'string':
-	// 		return value.length;
+		// value is a string; return string length
+		case 'string':
+			return value.length;
 
-	// 	// value is null or undefined; return `no value`
-	// 	case 'undefined':
-	// 		return 'no value';
+		// value is null or undefined; return `no value`
+		case 'undefined':
+			return 'no value';
 
-	// 	// true or false; return value
-	// 	case 'boolean':
-	// 		return value;
+		// true or false; return value
+		case 'boolean':
+			return value;
 
-	// 	// value is a number; return value in relation to 100
-	// 	case 'number':
-	// 		if (value < 100) {
-	// 			return 'less than 100';
-	// 		} else if (value > 100) {
-	// 			return 'more than 100';
-	// 		} else {
-	// 			return 'equal to 100';
-	// 		}
+		// value is a number; return value in relation to 100
+		case 'number':
+			if (value < 100) {
+				return 'less than 100';
+			} else if (value > 100) {
+				return 'more than 100';
+			} else {
+				return 'equal to 100';
+			}
 
 
-	// 	// call the function; run the function with true args
-	// 	case 'function':
-	// 		return value(true);
+		// call the function; run the function with true args
+		case 'function':
+			return value(true);
 
-	// 	default:
-	// 		// array and null returns an object
-	// 		// use Array.isArray to test for Array
-	// 		if (Array.isArray(value)) {
-	// 			if (value.length >= 3) {
-	// 				return value[2];
-	// 			}
-	// 			// return undefined if it doesnt exist
-	// 			return; 
+		default:
+			// array and null returns an object
+			// use Array.isArray to test for Array
+			if (Array.isArray(value)) {
+				if (value.length >= 3) {
+					return value[2];
+				}
+				// return undefined if it doesnt exist
+				return; 
 				
-	// 		} else { // value must null
-	// 			return  'no value';
-	// 		}
+			} else { // value must null
+				return  'no value';
+			}
 
-	// }
+	}
 }
 
 // Test Value
